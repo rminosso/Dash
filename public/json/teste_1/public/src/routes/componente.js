5753f64d81ccab8b6a9d879013551fdff4c3c8c1
MarleyS439 <marleysantos439@gmail.com>
2026-04-07 19:50:29 -0300

    Refatoração de nome de diretórios

--- public/json/teste_1/public/src/routes/componente.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var componenteController = require('../controllers/componenteController')
-
-router.get("/buscar", function (req, res) {
-    componenteController.buscar(req, res);
-});
-
-module.exports = router;
