6005e24a312ee16b866286df5475cf0e9c691e5c
MarleyS439 <marleysantos439@gmail.com>
2026-05-02 00:16:38 -0300

    Seção de relatórios

---
+++ public/assets/javascript/lottie.js
@@ -0,0 +1,15 @@
+import { DotLottie } from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
+
+new DotLottie({
+  autoplay: true,
+  loop: true,
+  canvas: document.getElementById("rel"),
+  src: "https://lottie.host/554c0b6a-1921-49b0-87d8-9f06018e1640/2CohfEOZNU.lottie",
+});
+
+new DotLottie({
+  autoplay: true,
+  loop: true,
+  canvas: document.getElementById("canvas1"),
+  src: "https://lottie.host/d479a913-8d60-446b-8aed-2c2256f81270/MNP158TUKh.lottie",
+});
